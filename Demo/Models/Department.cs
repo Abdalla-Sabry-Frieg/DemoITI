bad8cb1ec18e49f7803e9a55d466bcd1dafc7368
AbdallaSabry <sabryabdalla9@gmail.com>
2024-07-22 15:55:44 +0300

    Add project files.

---
+++ Demo/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Manager { get; set; }
+    }
+}
---
+++ Demo/Models/ITIEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Models
+{
+    public class ITIEntity:DbContext
+    {
+        public ITIEntity()
+        {
+        }
+        public ITIEntity(DbContextOptions options):base(options)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
